cf3b8144f0c8390461af17a99fcdae445c0435c7
djsaikrishna <ileanakavali4@gmail.com>
2023-07-25 23:13:15 +0530

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <Editor @onEncode="onEncode" />
+  </div>
+</template>
+
+<script>
+import Editor from '@/components/Editor.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Editor,
+  },
+  methods: {
+    onEncode() {
+      this.$emit('onEncode');
+    },
+  },
+};
+</script>
